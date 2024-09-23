3f3e8566b68f3562ef083079a52cd8ec78c4a797
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-09-23 09:53:26 +0200

    Hee hee hee haw void saker med void

--- Fighting spel (clash arena)/Program.cs
+++ Fighting spel (clash arena)/Program.cs
@@ -35,42 +35,46 @@ string EvilName = "Mini Pekka";
 Random generator = new Random();
 //Random generator som gör en ny random
 
+fight();
 while (ClasherHP > 0 && EvilHP > 0)
 //Loopa koden nedan om ditt HP eller fiendens HP är mer än 0 
-{
-    fight();
-    System.Console.WriteLine($"{EvilName} har {EvilHP} {ClasherName} har {ClasherHP}\n");
+{ 
+    System.Console.WriteLine($"{EvilName} har {EvilHP} hp {ClasherName} har {ClasherHP} hp \n");
 
     int ClasherDamage = generator.Next(20);
     EvilHP -= ClasherDamage;
     System.Console.WriteLine($"{ClasherName} gör {ClasherDamage} skada på {EvilName}");
-    System.Console.WriteLine($"{EvilName} har {EvilHP} {ClasherName} har {ClasherHP}\n");
+    System.Console.WriteLine($"{EvilName} har {EvilHP} hp {ClasherName} har {ClasherHP} hp \n");
 
     int EvilDamage = generator.Next(20);
     ClasherHP -= ClasherDamage;
     System.Console.WriteLine($"{EvilName} gör {EvilDamage} skada på {ClasherName}");
-    System.Console.WriteLine($"{EvilName} har {EvilHP} {ClasherName} har {ClasherHP}\n");
-    
+    System.Console.WriteLine($"{EvilName} har {EvilHP} hp {ClasherName} har {ClasherHP} hp \n");
+
     System.Console.WriteLine("Tryck på valfri knapp för att gå vidare");
     Console.ReadKey();
     
 }
 
-System.Console.WriteLine(finished);
+finished();
 
 if (ClasherHP == 0 && EvilHP == 0) 
 {
-     System.Console.WriteLine("DRAW");
+     draw();
     }
     else if (ClasherHP == 0) {
         System.Console.WriteLine($"{EvilName} vann (skill issue)");
+        winner();
     }
         else
         {
             System.Console.WriteLine($"{ClasherName} vann!");
+            winner();
+
+
         }
 
-System.Console.WriteLine("Tryck på en knapp för att bli klar");
+System.Console.WriteLine("Tryck på en knapp för exit");
 Console.ReadKey();
 
 void fight()
@@ -87,7 +91,8 @@ void fight()
 
 void finished() 
 {
-  System.Console.WriteLine(@" _______          ______         __    __        __    __        _______                      ______         __     __        ________        _______        
+  System.Console.WriteLine(@" 
+ _______          ______         __    __        __    __        _______                      ______         __     __        ________        _______        
 /       \        /      \       /  |  /  |      /  \  /  |      /       \                    /      \       /  |   /  |      /        |      /       \       
 $$$$$$$  |      /$$$$$$  |      $$ |  $$ |      $$  \ $$ |      $$$$$$$  |                  /$$$$$$  |      $$ |   $$ |      $$$$$$$$/       $$$$$$$  |      
 $$ |__$$ |      $$ |  $$ |      $$ |  $$ |      $$$  \$$ |      $$ |  $$ |                  $$ |  $$ |      $$ |   $$ |      $$ |__          $$ |__$$ |      
@@ -110,5 +115,54 @@ _______     .______              ___         ____    __    ____
 |  |  |  |   |      /          /  /_\  \        \            /      
 |  '--'  |   |  |\  \----.    /  _____  \        \    /\    /       
 |_______/    | _| `._____|   /__/     \__\        \__/  \__/        
-                                                                    ");
+                            ¯\_(T_T)_/¯                                               ");
+}
+// fin Ascii text om båda personerna har 0 hp
+
+// void Newround()
+// {
+//     System.Console.WriteLine(@" 
+//  _   _                                           _ 
+// | \ | | _____      __  _ __ ___  _   _ _ __   __| |
+// |  \| |/ _ \ \ /\ / / | '__/ _ \| | | | '_ \ / _` |
+// | |\  |  __/\ V  V /  | | | (_) | |_| | | | | (_| |
+// |_| \_|\___| \_/\_/   |_|  \___/ \__,_|_| |_|\__,_|");
+// }
+// //Ny runda för att det ska vara best of 3 (PS: Detta används inte än)
+void winner(){
+    System.Console.WriteLine(@"  
+  _   _ _____ _____   _   _ _____ _____   _   _ _____ _____   _   _    ___        __
+ | | | | ____| ____| | | | | ____| ____| | | | | ____| ____| | | | |  / \ \      / /
+ | |_| |  _| |  _|   | |_| |  _| |  _|   | |_| |  _| |  _|   | |_| | / _ \ \ /\ / / 
+ |  _  | |___| |___  |  _  | |___| |___  |  _  | |___| |___  |  _  |/ ___ \ V  V /  
+ |_| |_|_____|_____| |_| |_|_____|_____| |_| |_|_____|_____| |_| |_/_/   \_\_/\_/   
+                                                                                    
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠶⣶⠾⠿⢿⣇⢸⣿⣶⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣾⣿⣶⣿⡀⠸⠀⠀⢸⣿⢿⡇⠀⠈⠉⢻⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶
+⡿⠿⠿⠻⠿⠛⠛⠿⠿⠛⠛⠛⠛⠻⠛⢻⣿⡿⠋⢹⣿⣿⡇⠀⠀⠀⠈⢿⣿⡇⠀⠀⠀⣿⡿⠿⠿⠿⢿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⠿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣯⠀⠀⠀⠿⠟⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣦⠀⠀⠀⠀⢀⣀⣠⡤⠤⠤⠤⣄⣰⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣧⢀⡴⠚⠉⠁⠀⠀⠀⠀⠀⣠⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣦⣤⣀⣀⣤⣤⣤⣶⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣭⣭⣿⣿⣿⣿⣿⠿⢷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⣀⣀⣴⣿⣿⣴⣿⣿⣿⡍⠁⠀⠀⠀⣴⣶⣤⣿⣿⣷⣦⣄⡀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣴⣾⡿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⠿⠁⢀⣀⣀⡀⠻⢿⣿⣿⣿⣿⣿⠿⠿⠿⠿⠿⠿⠿⢿⣿⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⠀⣠⣾⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⢈⣿⡟⣋⡀⠀⠀⢰⡇⠀⠀⢸⠀⠀⠈⠉⠛⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣆⠀⠀⠀
+⠀⠀⢰⣿⡟⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣾⠋⣼⣿⣿⣆⠀⢸⡇⠀⠀⢸⠀⢀⣶⣿⣷⡘⣿⡀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠹⣿⣆⠀⠀
+⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⡇⠀⠀⠓⠺⢟⣀⣸⠃⠀⠀⢸⣀⣺⠿⠤⠈⠁⠘⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢻⣿⠀⠀
+⠀⠀⣿⡏⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣷⣶⣶⣿⡋⠁⠀⠀⠀⠀⠈⠉⠙⢿⣶⣶⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⣻⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⠟⠃⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⣴⣿⣿⣧⡀⠀⠀⢸⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⢸⡇⠀⠀⢀⡴⠻⣿⣷⡀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⣦⣼⣀⠀⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⢸⣀⣤⠶⠋⠀⠀⣿⣿⣿⡄⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⠟⠛⠛⠒⠻⠒⠒⠛⠛⠋⠉⠉⠀⠀⠀⠀⢸⣿⣿⣿⣿⡄⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠻⣿⣿⣿⣿⣿⣟⡛⠛⠁⠀⠀⠘⣠⣴⣾⣿⡗⢸⣿⣶⣤⣤⣄⠀⠀⠀⠀⣀⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠀⠘⢿⣿⣿⣿⣟⠳⢦⣄⣀⡀⠋⠉⠙⠛⠻⢧⣿⠟⠉⠁⠀⠀⣙⣠⡴⠞⣻⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⣹⣿⣿⣿⣦⡀⠀⣹⠙⠓⠒⠒⠒⠲⢶⡷⠖⠒⠒⠚⠋⢹⠁⢀⣴⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⣇⠀⠀⠀⠀⠀⢠⣿⠻⣿⣿⣿⣿⣦⣁⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⢈⣶⣿⣿⣿⣿⠟⠛⣿⡀⠀⠀⠀⠀⠀⢸⣿⠀⠀
+⠀⠀⣿⣿⡀⠀⠀⠀⢀⣿⣇⠀⠈⠻⣿⣿⣿⣿⣿⣶⣦⣤⣤⣤⣬⣤⣤⣤⣤⣶⣾⣿⣿⣿⡿⠟⠁⠀⠀⣿⣇⠀⠀⠀⠀⠀⣾⣿⠀⠀
+⠀⠀⢿⣿⣷⡀⠀⠀⣼⡟⠙⠻⢦⣤⣀⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠉⢀⣀⣴⠶⠟⠉⢿⡆⠀⠀⢀⣼⣿⣿⠀⠀
+⠀⠀⠘⢿⣿⣿⣦⣴⡿⠀⠀⠀⠀⠈⠙⠛⠶⣦⣄⣈⠛⠿⣿⣿⣿⣿⣿⣿⠿⢋⣁⣤⠶⠾⠛⠉⠀⠀⠀⠀⠸⣿⣀⣴⣾⣿⣿⠃⠀⠀
+⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠛⠛⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠉⠀⠀⠀⠀⠀⠀  ");
 }
--- Fighting spel (clash arena)/obj/Debug/net8.0/Fighting spel (clash arena).AssemblyInfo.cs
+++ Fighting spel (clash arena)/obj/Debug/net8.0/Fighting spel (clash arena).AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fighting spel (clash arena)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393c01f0ec7ede7f08bcf76b8a0ddddc7626913d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e603181a57825f3ca29d53ca895e47b560b674f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fighting spel (clash arena)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fighting spel (clash arena)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
